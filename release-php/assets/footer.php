94a633f04034c9c1a86b4d0062537c7acf113248
Luke Brown <me@luke.sx>
2018-07-19 18:38:26 +0100

    Updated to HTTPS

--- release-php/assets/footer.php
+++ release-php/assets/footer.php
@@ -1,30 +1,33 @@
-
-<div class="bg bg-grey"><div class="container">
+<div class="bg bg-grey">
+	<div class="container">
 		<div class="row footer">
 			<div class="col-md-11 col-xs-9">
 				<h3>Purchase this product and receive free lifetime updates</h3>
-				<p>Produced by Element Softworks Ltd, check out what else we make <a class="color-text" href="//elementsoftworks.co.uk">here</a>.</p>
+				<p>Produced by Element Softworks Ltd, check out what else we make
+					<a class="color-text" href="//elementsoftworks.co.uk">here</a>.
+				</p>
 			</div>
 			<div class="col-md-1 col-xs-3">
 				<img class="img-responsive" src="//i.imgur.com/qSikCu9.png">
 			</div>
 		</div>
-</div></div>
+	</div>
+</div>
 
-			</div>
-			<div id="color-picker">
-				<div class="row">
-					<div class="col-md-12 col-xs-5">
-						<span>Pick a color</span>
-					</div>
-					<div class="col-md-12 col-xs-7">
-						<div class="cp cp-red"    id="cp-red"></div>
-						<div class="cp cp-blue"   id="cp-blue"></div>
-						<div class="cp cp-green"  id="cp-green"></div>
-						<div class="cp cp-purple" id="cp-purple"></div>
-					</div>
-				</div>
-			</div>
-		<script type="text/javascript" src="assets/js/app.js"></script>
-	</body>
+</div>
+<div id="color-picker">
+	<div class="row">
+		<div class="col-md-12 col-xs-5">
+			<span>Pick a color</span>
+		</div>
+		<div class="col-md-12 col-xs-7">
+			<div class="cp cp-red" id="cp-red"></div>
+			<div class="cp cp-blue" id="cp-blue"></div>
+			<div class="cp cp-green" id="cp-green"></div>
+			<div class="cp cp-purple" id="cp-purple"></div>
+		</div>
+	</div>
+</div>
+<script type="text/javascript" src="assets/js/app.js"></script>
+</body>
 </html>
